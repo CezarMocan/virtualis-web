f0e16628cec09e3ec9a6a3326f2c1a077ec39c92
Cezar Mocan <mocancezar@gmail.com>
2020-08-18 22:53:36 -0400

    Basic menu

--- components/Menu.js
+++ components/Menu.js
@@ -4,21 +4,28 @@ import ShallowLink from './ShallowLink'
 import { withMainContext } from '../context/MainContext'
 
 class Main extends React.Component {
-  onJoin = () => {
-    this.props.router.push(this.props.router.pathname, `/join`, { shallow: true })
+  state = {
+    open: false
+  }
+  onMenuToggle = () => {
+    this.setState({ open: !this.state.open })
   }
   render() {
-    const { url } = this.props.url
+    const { url } = this.props
+    const { open } = this.state
     return (
       <div className="menu-container">
-        <ShallowLink href="/home"><a>Home</a></ShallowLink>        
-        <ShallowLink href="/join"><a>Join Portal</a></ShallowLink>
-        <ShallowLink href="/tours"><a>View Tours</a></ShallowLink>
-
-        {/* <Link href="/" shallow={true}> <a>Home</a> </Link>
-        <Link href="join" shallow={true}> <a>Join Portal</a> </Link>
-        <Link href="tours" shallow={true}> <a>View Tours</a> </Link>
-        <Link href="book" shallow={true}> <a>Book a tour</a> </Link> */}
+        <div className="menu-button" onClick={this.onMenuToggle}><h3 className="noselect">Menu</h3></div>
+        { open && 
+        <div className="menu-options-container">
+          <ShallowLink href="/home"><div className="menu-button menu-list-button"><h3 className="noselect">Home</h3></div></ShallowLink>
+          <ShallowLink href="/tours"><div className="menu-button menu-list-button"><h3 className="noselect">Tour Packages</h3></div></ShallowLink>
+          <ShallowLink href="/book"><div className="menu-button menu-list-button"><h3 className="noselect">Book a Tour</h3></div></ShallowLink>
+          <ShallowLink href="/about"><div className="menu-button menu-list-button"><h3 className="noselect">About</h3></div></ShallowLink>
+          <ShallowLink href="/join"><div className="menu-button menu-list-button"><h3 className="noselect">Join Portal</h3></div></ShallowLink>
+          <ShallowLink href="/faq"><div className="menu-button menu-list-button"><h3 className="noselect">FAQ</h3></div></ShallowLink>
+        </div>
+        }
       </div>
     )
   }
