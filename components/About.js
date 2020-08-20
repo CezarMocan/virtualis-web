51e4ebcafc034b6fbdc98c41a842c981585379a6
Cezar Mocan <mocancezar@gmail.com>
2020-08-19 23:51:26 -0400

    About page

--- components/About.js
+++ components/About.js
@@ -1,12 +1,75 @@
 import React from 'react'
 import { withMainContext } from '../context/MainContext'
+import ShallowLink from './ShallowLink'
+import Logo from './Logo'
+import MattImage from '../static/img/matt-image.png'
+import MattAvatar from '../static/img/matt-avatar.png'
+import LydiaImage from '../static/img/lydia-image.png'
+import LydiaAvatar from '../static/img/lydia-avatar.png'
 
 class Main extends React.Component {
   render() {
     const { url } = this.props.url
+    console.log('LI', LydiaImage)
     return (
       <div className="page-container">
-        <h1>About</h1>
+        <Logo className="small-logo"/>
+
+        <div className="page-content">
+          <div className="content-section narrow">
+            <p className="large centered">
+              The #1 VRChat Tour Agency!
+            </p>
+
+            <p className="large centered">
+              Virtualis is a creative experiment in art, research, and performance. Using the social VR platform VRChat, Matt and Lydia have created a conceptual tourist agency as a way to engage audiences with their research and fascinations with social VR. While being playful, performative, and fun, their tours are also critical examinations of the culture making, body confusions, and social norms specific to VRChat and prevalent in social VR as a medium. 
+            </p>
+
+            <p className="large centered">
+              Virtualis is the manifestion of the belief that media theory and deep thinking around the future of our digital spaces and counterparts can be explored and discussed outside of research papers and academic instituitions. Book a tour today and join us in exploring and questioning what social VR can teach us about the world today."
+            </p>
+
+          </div>
+
+          <div className="team-container">
+            <h1 className="centered">Meet Your Tour Guides</h1>
+
+            <div className="team-grid">
+              <div className="team-grid-item">
+
+                <div className="team-member-centered-image">
+                  <div className="team-member-image-container">
+                    <img className="team-member-image" src={LydiaImage}/>
+                    <img className="team-member-image avatar" src={LydiaAvatar}/>
+                  </div>
+                </div>
+
+                <h3 className="centered">Lydia Jessup</h3>
+
+                <p className="small">Matt Romein is an artist and performer whose work consists of live performance, generative computer art, and multi-media installation. His practice explores the ways in which the physical body is recreated and represented in digital spaces and how the digital body can be manipulated in evocative and unsettling ways that challenges ideas of identity, autonomy, and ethics. </p>
+                <p className="small">His work has been shown at BAM, Mana Contemporary, Soho Rep, The Public Theater, 3LD Art + Technology Center, and more. His art installation work has been shown at Sundance’s New Frontier Program, IDFA’s Doclab, and SXSW. He has had artist residencies and received grants from Pioneer Works, Google, CultureHub, Signal Culture, NYU, and more. He is currently a faculty member at ITP/IMA NYU in Brooklyn NY.</p>
+                <p className="small"><a href="https://www.lydiajessup.me/" target="_blank">https://www.lydiajessup.me/</a></p>
+              </div>
+              
+              <div className="team-grid-item">
+                <div className="team-member-centered-image">
+                  <div className="team-member-image-container">
+                    <img className="team-member-image" src={MattImage}/>
+                    <img className="team-member-image avatar" src={MattAvatar}/>
+                  </div>
+                </div>
+
+                <h3 className="centered">Matt Romein</h3>
+
+                <p className="small">Matt Romein is an artist and performer whose work consists of live performance, generative computer art, and multi-media installation. His practice explores the ways in which the physical body is recreated and represented in digital spaces and how the digital body can be manipulated in evocative and unsettling ways that challenges ideas of identity, autonomy, and ethics. </p>
+                <p className="small">His work has been shown at BAM, Mana Contemporary, Soho Rep, The Public Theater, 3LD Art + Technology Center, and more. His art installation work has been shown at Sundance’s New Frontier Program, IDFA’s Doclab, and SXSW. He has had artist residencies and received grants from Pioneer Works, Google, CultureHub, Signal Culture, NYU, and more. He is currently a faculty member at ITP/IMA NYU in Brooklyn NY.</p>
+                <p className="small"><a href="https://matt-romein.com/" target="_blank">https://matt-romein.com/</a></p>                
+              </div>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     )
   }
--- components/Book.js
+++ components/Book.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withMainContext } from '../context/MainContext'
+import ShallowLink from './ShallowLink'
 import Logo from './Logo'
 
 class Main extends React.Component {
@@ -11,17 +12,39 @@ class Main extends React.Component {
 
         <div className="page-content page-content-columns">
           <div className="page-column">
-            <h2>How to Book a Tour</h2>
+            <div className="content-section">
+              <h2>How to Book a Tour</h2>
+              <p>
+                Virtualis is presented as a part of IDFA DocLab's 2020 festival. Residents of Amsterdam NL can schedule an appointment through the DocLab website to come to the festival. Attendees will be provided with an Oculus Rift VR headset and a docent who will make the sure the equipment is working correctly and that you are outfitted properly. From there our knowledgable VR Tour Guides™ will guide you through an onboarding process before beginning the tour.
+              </p>
+              <p>
+                For those of you who are not residents of Amsterdam but have your own VR equipment we will have a select number of spots on each tour available for you. After registering we will send you details of what software you need as well as where and when to meet us.
+              </p>
+            </div>
 
-            <p>
-              Virtualis is presented as a part of IDFA DocLab's 2020 festival. Residents of Amsterdam NL can schedule an appointment through the DocLab website to come to the festival. Attendees will be provided with an Oculus Rift VR headset and a docent who will make the sure the equipment is working correctly and that you are outfitted properly. From there our knowledgable VR Tour Guides™ will guide you through an onboarding process before beginning the tour.
-            </p>
+            <div className="content-section">
+              <h2>Equipment Required</h2>
+              <p>
+                If you are attending in person through the festival there is no equipment need. Wear comfortable clothing and shoes. You will have the option to be seated or standing.
+              </p>
+              <p>
+                Remote attendees will need either an Oculus Rift headset or a Vive headset as a VR capable computer. Oculus Quest users will need to use a Link cable and PC. If you have questions about whether your VR equipment is compatible you may send us a message. 
+              </p>
+              <p>
+                While VRChat does have a non-VR desktop mode we will not be accomodating those users as we feel VR is an essential part of the experience and topics covered in our tours."
+              </p>
+            </div>
 
-            <p>
-              For those of you who are not residents of Amsterdam but have your own VR equipment we will have a select number of spots on each tour available for you. After registering we will send you details of what software you need as well as where and when to meet us.
-            </p>
+            <div className="content-section">
+              <h2>Can't Make a Tour?</h2>
+              <p>
+                Make sure to catch one of our live streams on Twitch or check out an archived tour.
+              </p>
+
+              <ShallowLink href="/join"><div className="menu-button small" style={{marginLeft: -5, marginTop: 5}}><h4 className="noselect">Watch Now</h4></div></ShallowLink>
+
+            </div>
           </div>
-
           <div className="page-column">
 
           </div>
